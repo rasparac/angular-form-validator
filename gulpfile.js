5cb5b6162c2c15f5d44c1b0361544215139c1af7
igor <borovica.igor@gmail.com>
2016-12-10 12:02:02 +0100

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+var src = './src/angular-form-validator.js';
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+    gulp.watch(src);
+});
+
+gulp.task('js-build', function() {
+    return gulp.src(src)
+        .pipe(uglify())
+        .pipe(concat('angular-form-validator.min.js'))
+        .pipe(gulp.dest('./dist/'));
+});
